81b0b0fe83e710de030916af6333a5d63b25fdcc
david.aponte <david.aponte@benevolent.ai>
2020-11-27 18:45:56 -0500

    feat: adding set

---
+++ non_linear/collections_set.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/golang-collections/collections/set"
+
+func main() {
+	set := set.New()
+	set.Insert(1)
+	set.Insert(2)
+	fmt.Println(set.Has(1))
+	set.Remove(1)
+	fmt.Println(set.Has(1))
+}
