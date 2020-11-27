7dfb74470f191a65d1fe19e66bb0c4b67609c93d
david.aponte <david.aponte@benevolent.ai>
2020-11-27 18:57:37 -0500

    feat: added counter

---
+++ non_linear/counter.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Counter map[interface{}]int
+
+func (c *Counter) Add(key interface{}) {
+	c[key] += 1
+}
+
+func (c *Counter) Find(key interface{}) bool {
+	_, ok := c[key]
+	return ok
+}
+
+func (c *Counter) Get(key interface{}) (int, bool) {
+	val, ok := c[key]
+	return val, ok
+}
+
+func main() {
+	counter := &Counter{}
+	counter.Add("a")
+	counter.Add("b")
+	counter.Add("a")
+	fmt.Println(counter.Find("a"))
+	fmt.Println(counter.Get("a"))
+}
