a7b7ada49e2aeb31602b3fb262ba661603cf4048
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-30 16:40:52 -0500

    chore: update example graph

--- non_linear/graph/adjacency_list_graphv2_test.go
+++ non_linear/graph/adjacency_list_graphv2_test.go
@@ -7,18 +7,25 @@ import (
 var g Graph
 
 func fillGraph() {
-	nA := GraphNode{"A"}
-	nB := GraphNode{"B"}
-	nC := GraphNode{"C"}
-	nD := GraphNode{"D"}
-	nE := GraphNode{"E"}
-	nF := GraphNode{"F"}
+	nA := GraphNode{"David"}
+	nB := GraphNode{"Pati"}
+	nC := GraphNode{"Ryan"}
+	nD := GraphNode{"Laura"}
+	nE := GraphNode{"Peter"}
+	nF := GraphNode{"Jason"}
+	nG := GraphNode{"Jon"}
+	nH := GraphNode{"Orlando"}
+	nI := GraphNode{"Dagmar"}
+
 	g.AddNode(&nA)
 	g.AddNode(&nB)
 	g.AddNode(&nC)
 	g.AddNode(&nD)
 	g.AddNode(&nE)
 	g.AddNode(&nF)
+	g.AddNode(&nG)
+	g.AddNode(&nH)
+	g.AddNode(&nI)
 
 	g.AddEdge(&nA, &nB)
 	g.AddEdge(&nA, &nC)
@@ -26,6 +33,11 @@ func fillGraph() {
 	g.AddEdge(&nC, &nE)
 	g.AddEdge(&nE, &nF)
 	g.AddEdge(&nD, &nA)
+	g.AddEdge(&nH, &nI)
+	g.AddEdge(&nH, &nF)
+	g.AddEdge(&nI, &nB)
+	g.AddEdge(&nG, &nF)
+
 }
 
 func TestGraph(t *testing.T) {
